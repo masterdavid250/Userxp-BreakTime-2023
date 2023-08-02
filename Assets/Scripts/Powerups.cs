5e23beca848bb32af35db951121a5d7ea000a4c9
AJ Galicia <aleynejasmin.galicia@benilde.edu.ph>
2023-08-02 16:30:20 +0800

    Prototype v6.1 (Changed powerup controls, "fixed" sticky paddle bug)

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Powerups : MonoBehaviour
 {
-    public GameObject               paddleMain;
-    public GameObject[]             paddleSides;
     public GameManager              gameManager;
     public Ball                     ball; 
-    [SerializeField] private float  scaleXMultiplier = 2f;
+    public GameObject               paddleMain;
+    public GameObject[]             paddleSides;
+    public Image[]                  powerupLights;  
     [SerializeField] private Sprite[] paddleSprites;
+    [SerializeField] private Sprite[] lightSprites;
+    [SerializeField] private float  scaleXMultiplier = 2f;
     private SpriteRenderer          paddleMainSpriteRenderer;
     private bool                    isBiggerPaddlesActivated = false;
     private bool                    isCurrencyChangeActivated = false;
@@ -21,12 +24,32 @@ public class Powerups : MonoBehaviour
         paddleMainSpriteRenderer = paddleMain.GetComponent<SpriteRenderer>();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Z))
+        {
+            BiggerPaddlePowerup();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.X))
+        {
+            CurrencyChange();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.C))
+        {
+            StickyPaddle();
+        }
+    }
+
     public void BiggerPaddlePowerup()
     {
         if (gameManager.Lives > 0 && !isBiggerPaddlesActivated)
         {
             gameManager.Lives--; 
             isBiggerPaddlesActivated = true;
+            powerupLights[0].GetComponent<Image>().sprite = lightSprites[1];
+
             Vector3 currentScale1 = paddleMain.transform.localScale;
             currentScale1.x *= scaleXMultiplier;
             paddleMain.transform.transform.localScale = currentScale1;
@@ -46,6 +69,8 @@ public class Powerups : MonoBehaviour
         {
             gameManager.Lives--;
             isCurrencyChangeActivated = true;
+            powerupLights[1].GetComponent<Image>().sprite = lightSprites[1];
+
             ball.ballDamage++;
             ball.spriteRenderer.sprite = ball.ballSprites[1];
             ball.UpdateColliderSize();
@@ -61,6 +86,8 @@ public class Powerups : MonoBehaviour
             // Other part of code for this is in the ball script
             gameManager.Lives--;
             isStickyPaddlesActivated = true;
+            powerupLights[2].GetComponent<Image>().sprite = lightSprites[1];
+
             paddleMainSpriteRenderer.sprite = paddleSprites[1];
             Invoke(nameof(ReturnStickyPaddleToNormal), 10);
         }
@@ -78,6 +105,7 @@ public class Powerups : MonoBehaviour
             paddleSides[i].transform.transform.localScale = currentScale2;
         }
         isBiggerPaddlesActivated = false;
+        powerupLights[0].GetComponent<Image>().sprite = lightSprites[0];
     }
 
     private void ReturnCurrencyToNormal() 
@@ -86,11 +114,13 @@ public class Powerups : MonoBehaviour
         isCurrencyChangeActivated = false;
         ball.spriteRenderer.sprite = ball.ballSprites[0];
         ball.UpdateColliderSize();
+        powerupLights[1].GetComponent<Image>().sprite = lightSprites[0];
     }
 
     private void ReturnStickyPaddleToNormal() 
     {
         paddleMainSpriteRenderer.sprite = paddleSprites[0];
         isStickyPaddlesActivated = false;
+        powerupLights[2].GetComponent<Image>().sprite = lightSprites[0];
     }
 }
